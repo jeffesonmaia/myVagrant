41539e4e0d9a658336f760bb6f0399e20424dab5
Evaldo Santos <evaldo@cdlfor.com.br>
2014-08-22 16:31:05 -0300

    Script para provisionamento

--- Vagrantfile
+++ Vagrantfile
@@ -20,11 +20,12 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   # Create a forwarded port mapping which allows access to a specific port
   # within the machine from a port on the host machine. In the example below,
   # accessing "localhost:8080" will access port 80 on the guest machine.
-  # config.vm.network "forwarded_port", guest: 80, host: 8080
+  config.vm.network "forwarded_port", guest: 80, host: 8080
+  config.vm.network "forwarded_port", guest: 3306, host: 3306
 
   # Create a private network, which allows host-only access to the machine
   # using a specific IP.
-  # config.vm.network "private_network", ip: "192.168.33.10"
+  config.vm.network "private_network", ip: "192.168.33.10"
 
   # Create a public network, which generally matched to bridged network.
   # Bridged networks make the machine appear as another physical device on
@@ -40,6 +41,7 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   # the path on the guest to mount the folder. And the optional third
   # argument is a set of non-required options.
   # config.vm.synced_folder "../data", "/vagrant_data"
+  config.vm.synced_folder "./www", "/var/www/html", :nfs => false, owner: "www-data", group: "www-data"
 
   # Provider-specific configuration so you can fine-tune various
   # backing providers for Vagrant. These expose provider-specific options.
--- install.sh
+++ install.sh
@@ -15,13 +15,16 @@ echo "--- Installing base packages ---"
 sudo apt-get install -y vim curl python-software-properties build-essential
 
 echo "--- MySQL time ---"
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password $DBPASSWD'
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password $DBPASSWD'
+sudo echo "mysql-server mysql-server/root_password password $DBPASSWD" | debconf-set-selections
+sudo echo "mysql-server mysql-server/root_password_again password $DBPASSWD" | debconf-set-selections
+
 sudo apt-get install mysql-server-5.5 -y
 
 echo -e "\n--- Setting up our MySQL user and db ---\n"
-mysql -uroot -p$DBPASSWD -e "CREATE DATABASE IF NOT EXISTS $DBNAME"
-mysql -uroot -p$DBPASSWD -e "grant all privileges on $DBNAME.* to '$DBUSER'@'$DBHOST' identified by '$DBPASSWD'"
+mysql -u$DBUSER -p$DBPASSWD -e "CREATE DATABASE IF NOT EXISTS $DBNAME"
+mysql -u$DBUSER -p$DBPASSWD -e "GRANT ALL PRIVILEGES ON *.* TO '$DBUSER'@'%' IDENTIFIED BY '$DBPASSWD' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+sed -i "s/bind-address.*/bind-address            = 0.0.0.0/" /etc/mysql/my.cnf
+sudo service mysql restart
 
 echo "--- We want the bleeding edge of PHP, right master? ---"
 sudo add-apt-repository -y ppa:ondrej/php5
@@ -30,15 +33,7 @@ echo "--- Updating packages list ---"
 sudo apt-get update
 
 echo "--- Installing PHP-specific packages ---"
-sudo apt-get install php5 -y
-
-echo "--- Installing PHPMYADMIN packages ---"
-sudo debconf-set-selections <<< 'phpmyadmin phpmyadmin/dbconfig-install boolean true'
-sudo debconf-set-selections <<< 'phpmyadmin phpmyadmin/app-password-confirm password $DBPASSWD'
-sudo debconf-set-selections <<< 'phpmyadmin phpmyadmin/mysql/admin-pass password $DBPASSWD'
-sudo debconf-set-selections <<< 'phpmyadmin phpmyadmin/mysql/app-pass password $DBPASSWD'
-sudo debconf-set-selections <<< 'phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2'﻿
-sudo apt-get install phpmyadmin -y
+sudo apt-get install php5 php5-mcrypt -y
 
 echo -e "\n--- Enabling mod-rewrite ---\n"
 sudo a2enmod rewrite > /dev/null 2>&1
@@ -49,11 +44,6 @@ sed -i "s/AllowOverride None/AllowOverride All/g" /etc/apache2/apache2.conf
 echo -e "\n--- We definitly need to see the PHP errors, turning them on ---\n"
 sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php5/apache2/php.ini
 sed -i "s/display_errors = .*/display_errors = On/" /etc/php5/apache2/php.ini
-
-echo -e "\n--- Configure Apache to use phpmyadmin ---\n"
-sudo ln -s /etc/phpmyadmin/apache.conf /etc/apache2/sites-available/phpmyadmin.conf
-sudo a2ensite phpmyadmin
-sudo service apache2 reload
 
 echo -e "\n--- Installing Composer for PHP package management ---\n"
 curl --silent https://getcomposer.org/installer | php > /dev/null 2>&1
@@ -70,4 +60,7 @@ mv composer.phar /usr/local/bin/composer
 #cd /tmp
 #rm -rf node-v0.10.31.tar.gz node-v0.10.31
 
-echo -e "\n--- Installing RVM ---\n"
+echo -e "\n--- Installing Node ---\n"
+sudo add-apt-repository ppa:chris-lea/node.js
+sudo apt-get update
+sudo apt-get install nodejs -y
