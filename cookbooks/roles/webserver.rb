1a7f7fc521a03966b322c32a0fc9f4a41e6192f8
Evaldo Santos <evaldojr2@gmail.com>
2014-11-23 09:18:41 -0300

    alteracao dos atributos defaults do mysql

--- cookbooks/roles/webserver.rb
+++ cookbooks/roles/webserver.rb
@@ -1,6 +1,19 @@
 name "webserver"
 description "Role for web servers"
 
+override_attributes(
+  'mysql' => {
+  	'server_root_password' => '1234',
+	'server_debian_password' => '1234',
+	'server_repl_password' => '1234',
+    'port' => '3306',
+    'data_dir' => '/opt/local/lib/mysql',
+    'allow_remote_root' => true,
+    'remove_anonymous_users' => true,
+    'remove_test_database'=> true
+  }
+)
+
 run_list(
   "recipe[apt]",
   "recipe[vim]",
